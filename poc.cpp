5acb936bb4ac89afc5217a90f77fdb50839f81a6
Eduardo Costa <m4c0@users.noreply.github.com>
2023-11-11 12:41:45 +0000

    moving constants around

--- poc.cpp
+++ poc.cpp
@@ -62,38 +62,36 @@ public:
 };
 
 using namespace nessa::midi;
+static constexpr const auto notes_per_beat = 2.0;
+static constexpr const auto ms_per_note = 1000.0 / (bps * notes_per_beat);
+
+static constexpr const auto note_count = 32;
+static constexpr const midi::note inst_1[note_count] = {
+    E4, EXTEND, B3, C4,     D4, EXTEND, C4,   B3,     //
+    A3, EXTEND, A3, C4,     E4, EXTEND, D4,   C4,     //
+    B3, EXTEND, B3, C4,     D4, EXTEND, E4,   EXTEND, //
+    C4, EXTEND, A3, EXTEND, A3, EXTEND, MUTE, MUTE,   //
+};
+static constexpr const midi::note inst_2[note_count] = {
+    MUTE, MUTE,   Ab3,  MUTE, MUTE, MUTE,   A3,   Ab3,  //
+    E3,   EXTEND, MUTE, MUTE, MUTE, MUTE,   MUTE, MUTE, //
+    Ab3,  EXTEND, Ab3,  A3,   B3,   EXTEND, MUTE, MUTE, //
+    MUTE, MUTE,   MUTE, MUTE, MUTE, MUTE,   MUTE, MUTE, //
+};
+static constexpr const midi::note inst_3[note_count] = {
+    E3,  E4,  E3,  E4,  E3,  E4,  E3,  E4,  //
+    A2,  A3,  A2,  A3,  A2,  A3,  A2,  A3,  //
+    Ab2, Ab3, Ab2, Ab3, Ab2, Ab3, Ab2, Ab3, //
+    A2,  A3,  A2,  A3,  A2,  A3,  A2,  A3,  //
+};
+static constexpr const midi::note inst_4[note_count] = {
+    MUTE, C5, MUTE, C5, MUTE, C5, MUTE, C5, //
+    MUTE, C5, MUTE, C5, MUTE, C5, MUTE, C5, //
+    MUTE, C5, MUTE, C5, MUTE, C5, MUTE, C5, //
+    MUTE, C5, MUTE, C5, MUTE, C5, MUTE, C5, //
+};
+
 class thread : sith::thread {
-  static constexpr const auto bpm = 140.0;
-  static constexpr const auto bps = bpm / 60.0;
-  static constexpr const auto notes_per_beat = 2.0;
-  static constexpr const auto ms_per_note = 1000.0 / (bps * notes_per_beat);
-
-  static constexpr const auto note_count = 32;
-  static constexpr const midi::note inst_1[note_count] = {
-      E4, EXTEND, B3, C4,     D4, EXTEND, C4,   B3,     //
-      A3, EXTEND, A3, C4,     E4, EXTEND, D4,   C4,     //
-      B3, EXTEND, B3, C4,     D4, EXTEND, E4,   EXTEND, //
-      C4, EXTEND, A3, EXTEND, A3, EXTEND, MUTE, MUTE,   //
-  };
-  static constexpr const midi::note inst_2[note_count] = {
-      MUTE, MUTE,   Ab3,  MUTE, MUTE, MUTE,   A3,   Ab3,  //
-      E3,   EXTEND, MUTE, MUTE, MUTE, MUTE,   MUTE, MUTE, //
-      Ab3,  EXTEND, Ab3,  A3,   B3,   EXTEND, MUTE, MUTE, //
-      MUTE, MUTE,   MUTE, MUTE, MUTE, MUTE,   MUTE, MUTE, //
-  };
-  static constexpr const midi::note inst_3[note_count] = {
-      E3,  E4,  E3,  E4,  E3,  E4,  E3,  E4,  //
-      A2,  A3,  A2,  A3,  A2,  A3,  A2,  A3,  //
-      Ab2, Ab3, Ab2, Ab3, Ab2, Ab3, Ab2, Ab3, //
-      A2,  A3,  A2,  A3,  A2,  A3,  A2,  A3,  //
-  };
-  static constexpr const midi::note inst_4[note_count] = {
-      MUTE, C5, MUTE, C5, MUTE, C5, MUTE, C5, //
-      MUTE, C5, MUTE, C5, MUTE, C5, MUTE, C5, //
-      MUTE, C5, MUTE, C5, MUTE, C5, MUTE, C5, //
-      MUTE, C5, MUTE, C5, MUTE, C5, MUTE, C5, //
-  };
-
 public:
   using sith::thread::start;
 
